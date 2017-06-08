8c25df80e79681be49ea46bebfcacf746b8903d4
Tomoyuki OHNO <ohno.tomoyuki@ricksoft.jp>
2017-06-08 18:00:34 +0900

    Fixtureを更新

--- Test/Fixture/MultidatabaseFrameSettingFixture.php
+++ Test/Fixture/MultidatabaseFrameSettingFixture.php
@@ -15,45 +15,131 @@
 class MultidatabaseFrameSettingFixture extends CakeTestFixture {
 
 /**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
-		'frame_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'フレームKey', 'charset' => 'utf8'),
-		'content_per_page' => array('type' => 'integer', 'null' => false, 'default' => '10', 'unsigned' => false, 'comment' => '表示件数 1件,5件,10件,20件,50件,100件'),
-		'default_sort_type' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 4, 'unsigned' => false, 'comment' => '表示順 0:登録日時,1:更新日時,2:メタデータ'),
-		'default_sort_order' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => '並び順 0:昇順,1:降順'),
-		'multidatabase_metadata_sort_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'comment' => 'ソートキーとするメタデータID'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '作成者'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '更新者'),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
-
-/**
  * Records
  *
  * @var array
  */
-	public $records = array(
-		array(
-			'id' => 1,
-			'frame_key' => 'Lorem ipsum dolor sit amet',
-			'content_per_page' => 1,
-			'default_sort_type' => 1,
-			'default_sort_order' => 1,
-			'multidatabase_metadata_sort_id' => 1,
-			'created_user' => 1,
-			'created' => '2016-11-17 09:20:12',
-			'modified_user' => 1,
-			'modified' => '2016-11-17 09:20:12'
-		),
-	);
+	public $records = [
+		[
+			'id' => '1',
+			'frame_key' => 'frame_key_1',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '1',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '1',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '2',
+			'frame_key' => 'frame_key_2',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '1',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '1',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '3',
+			'frame_key' => 'frame_key_3',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '1',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '1',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '4',
+			'frame_key' => 'frame_key_4',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '1',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '1',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '5',
+			'frame_key' => 'frame_key_5',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '1',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '1',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '6',
+			'frame_key' => 'frame_key_6',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '2',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '2',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '7',
+			'frame_key' => 'frame_key_7',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '2',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '2',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '8',
+			'frame_key' => 'frame_key_8',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '2',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '2',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '9',
+			'frame_key' => 'frame_key_9',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '2',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '2',
+			'modified' => '2016/04/01 1:10:20',
+		],
+		[
+			'id' => '10',
+			'frame_key' => 'frame_key_10',
+			'content_per_page' => '5',
+			'default_sort_type' => '',
+			'default_sort_order' => '1',
+			'created_user' => '2',
+			'created' => '2016/04/01 1:10:20',
+			'modified_user' => '2',
+			'modified' => '2016/04/01 1:10:20',
+		],
+	];
 
+	/**
+	 * Initialize the fixture.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		require_once App::pluginPath('Links') . 'Config' . DS . 'Schema' . DS . 'schema.php';
+		$this->fields = (new LinksSchema())->tables[Inflector::tableize($this->name)];
+		parent::init();
+	}
 }
--- Test/Fixture/MultidatabaseMetadataSettingFixture.php
+++ Test/Fixture/MultidatabaseMetadataSettingFixture.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * MultidatabaseFixture
+ * MultidatabaseMetadataSettingFixture
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Tomoyuki OHNO (Ricksoft Co., Ltd.) <ohno.tomoyuki@ricksoft.jp>
@@ -10,9 +10,16 @@
  */
 
 /**
- * Summary for MultidatabaseFixture
+ * Summary for MultidatabaseMetadataSettingFixture
  */
-class MultidatabaseFixture extends CakeTestFixture {
+class MultidatabaseMetadataSettingFixture extends CakeTestFixture {
+
+/**
+ * Table name
+ *
+ * @var string
+ */
+	public $table = 'multidatabase_metadata_settings';
 
 /**
  * Records
@@ -24,7 +31,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '1',
 			'block_id' => '1',
 			'key' => 'multidatabase_1',
-			'name' => 'Test Multidatabase 1',
+			'auto_number_sequence' => '0',
 			'created_user' => '1',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '1',
@@ -34,7 +41,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '2',
 			'block_id' => '2',
 			'key' => 'multidatabase_2',
-			'name' => 'Test Multidatabase 2',
+			'auto_number_sequence' => '0',
 			'created_user' => '1',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '1',
@@ -44,7 +51,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '3',
 			'block_id' => '3',
 			'key' => 'multidatabase_3',
-			'name' => 'Test Multidatabase 3',
+			'auto_number_sequence' => '0',
 			'created_user' => '1',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '1',
@@ -54,7 +61,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '4',
 			'block_id' => '4',
 			'key' => 'multidatabase_4',
-			'name' => 'Test Multidatabase 4',
+			'auto_number_sequence' => '0',
 			'created_user' => '1',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '1',
@@ -64,7 +71,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '5',
 			'block_id' => '5',
 			'key' => 'multidatabase_5',
-			'name' => 'Test Multidatabase 5',
+			'auto_number_sequence' => '0',
 			'created_user' => '1',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '1',
@@ -74,7 +81,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '6',
 			'block_id' => '6',
 			'key' => 'multidatabase_6',
-			'name' => 'Test Multidatabase 6',
+			'auto_number_sequence' => '0',
 			'created_user' => '2',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '2',
@@ -84,7 +91,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '7',
 			'block_id' => '7',
 			'key' => 'multidatabase_7',
-			'name' => 'Test Multidatabase 7',
+			'auto_number_sequence' => '0',
 			'created_user' => '2',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '2',
@@ -94,7 +101,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '8',
 			'block_id' => '8',
 			'key' => 'multidatabase_8',
-			'name' => 'Test Multidatabase 8',
+			'auto_number_sequence' => '0',
 			'created_user' => '2',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '2',
@@ -104,7 +111,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '9',
 			'block_id' => '9',
 			'key' => 'multidatabase_9',
-			'name' => 'Test Multidatabase 9',
+			'auto_number_sequence' => '0',
 			'created_user' => '2',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '2',
@@ -114,7 +121,7 @@ class MultidatabaseFixture extends CakeTestFixture {
 			'id' => '10',
 			'block_id' => '10',
 			'key' => 'multidatabase_10',
-			'name' => 'Test Multidatabase 10',
+			'auto_number_sequence' => '0',
 			'created_user' => '2',
 			'created' => '2016/04/01 1:10:20',
 			'modified_user' => '2',
