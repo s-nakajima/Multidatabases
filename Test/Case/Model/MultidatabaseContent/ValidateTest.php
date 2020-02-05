550d050a40491c79687b65ba8835df3fe371e165
Ryuji AMANO <ryuji@ryus.co.jp>
2020-02-05 16:02:37 +0900

    fix: 汎用DBコンテンツへのファイル添付時に、NC3で許可してない拡張子のファイルだと内部エラーになるのを修正

---
+++ Test/Case/Model/MultidatabaseContent/ValidateTest.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * ValidateTest.php
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
